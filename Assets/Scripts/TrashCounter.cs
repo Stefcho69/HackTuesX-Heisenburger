015969ca0a361cb681cf1aa137c2439aa8dd6d91
krasavecotklasa <cegrig4@gmail.com>
2024-03-15 19:57:11 +0200

    scripts

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TrashCounter : MonoBehaviour
+{   
+    public static TrashCounter instance;
+    public TMP_Text trashtext;
+    public int currenttrash = 0;
+    private void Awake()
+    {
+        instance = this;
+
+    }
+    void Start()
+    {
+        trashtext.text = "Trash: " + currenttrash.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseTrash(int v)
+    {
+        currenttrash += v;
+        trashtext.text = "Trash: " + currenttrash.ToString();
+    }
+}
