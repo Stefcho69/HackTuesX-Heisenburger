cd60aada70e4575075fc861a6ba2033f7a22623c
Stefcho69 <stefan08vss@gmail.com>
2024-03-15 18:51:54 +0200

    bokluk

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public int value;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            TrashCounter.instance.IncreaseTrash(value);
+        }
+    }
+}
